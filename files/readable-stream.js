deabdbec344b5ab4710476fddb7f65d13b495525
SirWilliamIII <dev.wtc3@gmail.com>
2017-10-17 11:35:06 -0600

    returns before pushing fixing EOL error

--- files/readable-stream.js
+++ files/readable-stream.js
@@ -3,14 +3,14 @@ const { Readable } = require('stream')
 const inStream = new Readable({
     read(size) {
         setTimeout(() => {
-            this.push(String.fromCharCode(this.currentCharCode++))
             if(this.currentCharCode > 90) {
                 this.push(null)
+                return
             }
+            this.push(String.fromCharCode(this.currentCharCode++))
         }, 200)
     }
 })
 
 inStream.currentCharCode = 65
 inStream.pipe(process.stdout)
-
